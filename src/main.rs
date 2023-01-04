87b9741b2b10ea4285950b4165602a65dd36fdbb
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-01-04 17:29:44 +0100

    chore: add Transaction struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 extern crate serde_json;
 
-use serde_json::Value as JsonValue;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Transaction {
+    transaction_type: String,
+    contract_name: String,
+    contract_address: String,
+}
 
 fn main() {
     let foundry_broadcast = r#"
@@ -10,17 +18,6 @@ fn main() {
             "contractAddress": "0xD1aE64401d65E9B0d1bF7E08Fbf75bb2F26eF70a"
         }
     "#;
-
-    let deserialized_result = serde_json::from_str(foundry_broadcast);
-
-    if deserialized_result.is_ok() {
-        let parsed_result: JsonValue = deserialized_result.unwrap();
-        println!("Type of transaction: {}", parsed_result["transactionType"]);
-        println!("Name of the contract: {}", parsed_result["contractName"]);
-        println!("Deployed to: {}", parsed_result["contractAddress"]);
-    } else {
-        println!("Sorry, could not parse JSON.");
-    }
 }
 
 mod serialization_tests {
