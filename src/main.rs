9d736ae93edd5396af70d7b7f877690f5537d0c4
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-01-04 15:51:57 +0100

    chore: add example foundry broadcast string

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,11 @@ extern crate serde_json;
 use serde_json::Value as JsonValue;
 
 fn main() {
-    println!("Hello, world!");
+    let foundry_broadcast = r#"
+        {
+            "transactionType": "CREATE",
+            "contractName": "Counter",
+            "contractAddress": "0xD1aE64401d65E9B0d1bF7E08Fbf75bb2F26eF70a",
+        }
+    "#;
 }
