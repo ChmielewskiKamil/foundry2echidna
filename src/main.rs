bb6fe3606355abf571e7241ae486c44f0fb6f2a8
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-01-08 00:49:35 +0100

    chore: experiment

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,48 @@
-use serde::{Deserialize, Serialize};
+extern crate serde_json;
 
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct Transaction {
-    transaction_type: String,
-    contract_name: String,
-    contract_address: String,
-}
+use serde_json::{json, Result, Value};
+use std::fs::File;
+use std::io::prelude::*;
 
-fn main() {
-    let foundry_broadcast = r#"
-        {
-            "transactionType": "CREATE",
-            "contractName": "Counter",
-            "contractAddress": "0xD1aE64401d65E9B0d1bF7E08Fbf75bb2F26eF70a"
-        }
-    "#;
+fn main() -> Result<()> {
+    // Open the file in read-only mode (ignoring errors).
+    let mut file = File::open("test_broadcast.json").unwrap();
+    // Read the contents of the file into a string.
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
 
-    let transaction = parse_foundry_broadcast(foundry_broadcast);
-    println!("{:?}", transaction);
-}
+    // Parse the string of data into serde_json::Value.
+    let v: Value = serde_json::from_str(&contents)?;
+    // println!("{:?}", v);
 
-fn parse_foundry_broadcast(broadcast: &str) -> Transaction {
-    let parsed: Transaction = serde_json::from_str(broadcast).unwrap();
-    parsed
-}
-
-#[cfg(test)]
-mod deserialization_tests {
-
-    use super::parse_foundry_broadcast;
-
-    #[test]
-    fn it_should_parse_transaction_type_correctly() {
-        assert_eq!(
-            parse_foundry_broadcast(
-                r#"
-            {
-            "transactionType": "CREATE",
-            "contractName": "Counter",
-            "contractAddress": "0xD1aE64401d65E9B0d1bF7E08Fbf75bb2F26eF70a"
-        }
-        "#
-            )
-            .transaction_type,
-            "CREATE"
-        );
+    // Manipulate the data as needed
+    let mut events = Vec::new();
+    for txn in v["transactions"].as_array().unwrap() {
+        let event = match txn["transactionType"].as_str().unwrap() {
+            "CREATE" => "ContractCreated",
+            "CALL" => "ContractCalled",
+            _ => "Unknown",
+        };
+        let address = txn["contractAddress"].as_str().unwrap();
+        let from = txn["transaction"]["from"].as_str().unwrap();
+        let data = txn["transaction"]["data"].as_str().unwrap();
+        let value = txn["transaction"]["value"].as_str().unwrap();
+        let gas_used = txn["transaction"]["gas"].as_str().unwrap();
+        // let gas_price = txn["transaction"]["gasPrice"].as_str().unwrap();
+        let event_obj = json!({
+            "event": event,
+            "from": from,
+            "contract_address": address,
+            "data": data,
+            "value": value,
+            "gas_used": gas_used,
+            // "gas_price": gas_price,
+        });
+        events.push(event_obj);
     }
 
-    #[test]
-    fn it_should_parse_contract_name_correctly() {
-        assert_eq!(
-            parse_foundry_broadcast(
-                r#"
-            {
-            "transactionType": "CREATE",
-            "contractName": "Counter",
-            "contractAddress": "0xD1aE64401d65E9B0d1bF7E08Fbf75bb2F26eF70a"
-        }
-        "#
-            )
-            .contract_name,
-            "Counter"
-        );
-    }
+    // Serialize the data back into a JSON string.
+    let j = serde_json::to_string(&events)?;
+    println!("{}", j);
+    Ok(())
 }
