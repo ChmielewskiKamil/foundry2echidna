2272a09f53a790b76552a691abeb034ca13a5b50
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-01-11 22:33:04 +0100

    test: reformat expected content in 1st test

--- src/lib.rs
+++ src/lib.rs
@@ -35,20 +35,22 @@ mod parser_tests {
 
     #[test]
     fn it_should_read_file_content_to_string() {
-        let expected_content = r#"{
-    "transactions": [
+        let expected_content = r#"
         {
-            "transactionType": "CREATE",
-            "contractAddress": "0x057ef64E23666F000b34aE31332854aCBd1c8544",
-            "transaction": {
-                "from": "0x90f79bf6eb2c4f870365e785982e1f101e93b906",
-                "gas": "0x8f864",
-                "value": "0x0",
-                "data": "0x6080604"
-            }
+            "transactions": [
+                {
+                    "transactionType": "CREATE",
+                    "contractAddress": "0x057ef64E23666F000b34aE31332854aCBd1c8544",
+                    "transaction": {
+                        "from": "0x90f79bf6eb2c4f870365e785982e1f101e93b906",
+                        "gas": "0x8f864",
+                        "value": "0x0",
+                        "data": "0x6080604"
+                    }
+                }
+            ]
         }
-    ]
-}"#;
+"#;
 
         let actual_content = read_broadcast_file("test_json_files/simple_broadcast_test.json");
         assert_eq!(expected_content, actual_content.unwrap());
