5f6d65d823153789f28c1313b6edaaff84db08e5
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-01-12 00:17:55 +0100

    test: single receipt deserialize test

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,15 @@ struct TransactionDetails {
     data: String,
 }
 
+#[derive(Deserialize, Debug, PartialEq)]
+struct Receipt {
+    from: String,
+    to: Option<String>,
+    contract_address: String,
+    gas_used: String,
+    effective_gas_price: String,
+}
+
 pub fn read_broadcast_file(path_to_file: &str) -> Result<String, String> {
     let mut file =
         File::open(path_to_file).map_err(|err| format!("Error while opening the file: {}", err))?;
@@ -34,6 +43,10 @@ fn deserialize_single_transaction(
     let transaction: Transaction = serde_json::from_str(&transaction_to_deserialize)
         .map_err(|err| format!("Failed to deserialize transaction: {}", err))?;
     Ok(transaction)
+}
+
+fn deserialize_single_receipt(receipt_to_deserialize: String) -> Receipt {
+    todo!()
 }
 
 #[cfg(test)]
@@ -91,5 +104,36 @@ mod parser_tests {
             },
         };
         assert_eq!(deserialized_transaction, expected_deserialization_result);
+    }
+
+    #[test]
+    fn it_should_properly_deserialize_single_receipt() {
+        let receipt_to_deserialize = r#"
+        {
+            "transactionHash": "0xd532ff21e93eac89c2bbd5f4813ac0d9274e479b6eb09b2b2f45b82489faba1b",
+            "transactionIndex": "0x0",
+            "blockHash": "0xec94f9df892826b801574831de293f983ed8f3f81036a99faa616a8da694b2a9",
+            "blockNumber": "0x1",
+            "from": "0x90F79bf6EB2c4f870365E785982E1f101E93b906",
+            "to": null,
+            "cumulativeGasUsed": "0x1234",
+            "gasUsed": "0x6e675",
+            "contractAddress": "0x057ef64E23666F000b34aE31332854aCBd1c8544",
+            "effectiveGasPrice": "0xe0fed783"
+        },
+        "#
+        .to_string();
+
+        let deserialized_receipt: Receipt = deserialize_single_receipt(receipt_to_deserialize);
+
+        let expected_deserialization_result = Receipt {
+            from: "0x90F79bf6EB2c4f870365E785982E1f101E93b906".to_string(),
+            to: None,
+            contract_address: "0x057ef64E23666F000b34aE31332854aCBd1c8544".to_string(),
+            gas_used: "0x6e675".to_string(),
+            effective_gas_price: "0xe0fed783".to_string(),
+        };
+
+        assert_eq!(deserialized_receipt, expected_deserialization_result);
     }
 }
