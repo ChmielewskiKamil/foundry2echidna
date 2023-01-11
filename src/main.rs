eae75934b26dc7341c615d0f46041149b4733187
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-01-11 21:41:30 +0100

    chore: add unwrap in main to satisfy clippy

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use foundry2echidna::read_broadcast_file;
 
 fn main() {
-    read_broadcast_file("test_json_files/simple_broadcast_test.json");
+    read_broadcast_file("test_json_files/simple_broadcast_test.json").unwrap();
 }
