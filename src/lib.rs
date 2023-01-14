6fcfe2a19c1fdd0e8210f9a31ae7924b763bffdd
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-01-14 10:13:57 +0100

    test: fix gas price typo in expected result

--- src/lib.rs
+++ src/lib.rs
@@ -240,7 +240,7 @@ mod parser_tests {
             effective_gas_price: "0xe0fed783".to_string(),
         };
 
-        let expected_serialization_result = r#"{"event":"FunctionCall","from":"0x90f79bf6eb2c4f870365e785982e1f101e93b906","to":"0x057ef64e23666f000b34ae31332854acbd1c8544","gas_used":"0x6e675","gas_price":"0xe0fed783","data":"0x6080604","value":"0x0"}"#;
+        let expected_serialization_result = r#"{"event":"FunctionCall","from":"0x90f79bf6eb2c4f870365e785982e1f101e93b906","to":"0x057ef64e23666f000b34ae31332854acbd1c8544","gas_used":"0xb3bd","gas_price":"0xe0fed783","data":"0x202023","value":"0x0"}"#;
 
         let serialization_result =
             serialize_transaction(transaction_to_serialize, receipt_to_serialize);
