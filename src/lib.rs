bbcf4f2e8b4668e6db0d941ddbd487a65704ddb3
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-01-14 10:24:15 +0100

    test: update test for error handling

--- src/lib.rs
+++ src/lib.rs
@@ -217,7 +217,7 @@ mod parser_tests {
         let expected_serialization_result = r#"{"event":"ContractCreated","from":"0x90f79bf6eb2c4f870365e785982e1f101e93b906","contract_address":"0x057ef64E23666F000b34aE31332854aCBd1c8544","gas_used":"0x6e675","gas_price":"0xe0fed783","data":"0x6080604","value":"0x0"}"#;
 
         let serialization_result =
-            serialize_transaction(transaction_to_serialize, receipt_to_serialize);
+            serialize_transaction(transaction_to_serialize, receipt_to_serialize).unwrap();
 
         assert_eq!(expected_serialization_result, serialization_result);
     }
