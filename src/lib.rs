3abba5fda42b5809c407ce15a2a5aa97c09e5e06
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-01-27 23:31:28 +0100

    fix: remove unnecessary levels of complexity
    test: fix expected data to match broadcast
    
    chore: simplify return type, remove List structs
    
    test: simplify return type to match serial func
    
    chore: return Broadcast instead of tuple

--- src/lib.rs
+++ src/lib.rs
@@ -59,18 +59,18 @@ struct FunctionCallEvent {
 ////////////////////////////////////////////////////////////// */
 #[derive(Deserialize, Debug, PartialEq)]
 struct TransactionsList {
-    transactions: Vec<String>,
+    transactions: Vec<Transaction>,
 }
 
 #[derive(Deserialize, Debug, PartialEq)]
 struct ReceiptsList {
-    receipts: Vec<String>,
+    receipts: Vec<Receipt>,
 }
 
 #[derive(Deserialize, Debug, PartialEq)]
 struct Broadcast {
-    transactions: Vec<String>,
-    receipts: Vec<String>,
+    transactions: Vec<Transaction>,
+    receipts: Vec<Receipt>,
 }
 
 /*//////////////////////////////////////////////////////////////
@@ -101,46 +101,38 @@ fn deserialize_single_receipt(receipt_to_deserialize: &str) -> Result<Receipt, S
     Ok(receipt)
 }
 
-#[allow(dead_code)]
-fn deserialize_multiple_transactions(
-    transactions_to_deserialize: TransactionsList,
-) -> Result<Vec<Transaction>, String> {
-    let mut transactions = vec![];
-    for transaction in transactions_to_deserialize.transactions.into_iter() {
-        transactions.push(
-            deserialize_single_transaction(&transaction)
-                .map_err(|err| format!("Failed to deserialze an array of transactions: {err}"))?,
-        );
-    }
-    Ok(transactions)
-}
+// #[allow(dead_code)]
+// fn deserialize_multiple_transactions(
+//     transactions_to_deserialize: TransactionsList,
+// ) -> Result<Vec<Transaction>, String> {
+//     let mut transactions = vec![];
+//     for transaction in transactions_to_deserialize.transactions.into_iter() {
+//         transactions.push(
+//             deserialize_single_transaction(&transaction)
+//                 .map_err(|err| format!("Failed to deserialze an array of transactions: {err}"))?,
+//         );
+//     }
+//     Ok(transactions)
+// }
 
-#[allow(dead_code)]
-fn deserialize_multiple_receipts(
-    receipts_to_deserialize: ReceiptsList,
-) -> Result<Vec<Receipt>, String> {
-    let mut receipts = vec![];
-    for receipt in receipts_to_deserialize.receipts.into_iter() {
-        receipts.push(
-            deserialize_single_receipt(&receipt)
-                .map_err(|err| format!("Failed to deserialize an array of receipts: {err}"))?,
-        );
-    }
-    Ok(receipts)
-}
+// #[allow(dead_code)]
+// fn deserialize_multiple_receipts(
+//     receipts_to_deserialize: ReceiptsList,
+// ) -> Result<Vec<Receipt>, String> {
+//     let mut receipts = vec![];
+//     for receipt in receipts_to_deserialize.receipts.into_iter() {
+//         receipts.push(
+//             deserialize_single_receipt(&receipt)
+//                 .map_err(|err| format!("Failed to deserialize an array of receipts: {err}"))?,
+//         );
+//     }
+//     Ok(receipts)
+// }
 
-fn deserialize_broadcast(
-    broadcast_to_deserialize: &str,
-) -> Result<(TransactionsList, ReceiptsList), String> {
+fn deserialize_broadcast(broadcast_to_deserialize: &str) -> Result<Broadcast, String> {
     let broadcast: Broadcast = serde_json::from_str(broadcast_to_deserialize)
         .map_err(|err| format!("Failed to deserialize broadcast: {}", err))?;
-    let transactions = TransactionsList {
-        transactions: broadcast.transactions,
-    };
-    let receipts = ReceiptsList {
-        receipts: broadcast.receipts,
-    };
-    Ok((transactions, receipts))
+    Ok(broadcast)
 }
 
 /*//////////////////////////////////////////////////////////////
@@ -292,165 +284,165 @@ mod parser_tests {
 
         assert_eq!(deserialized_receipt, expected_deserialization_result);
     }
-    #[test]
-    fn it_should_deserialize_a_series_of_transactions() {
-        let tx1 = r#"{
-            "hash": "0xd532ff21e93eac89c2bbd5f4813ac0d9274e479b6eb09b2b2f45b82489faba1b",
-            "transactionType": "CREATE",
-            "contractName": "Ethernaut",
-            "contractAddress": "0x057ef64E23666F000b34aE31332854aCBd1c8544",
-            "function": null,
-            "arguments": null,
-            "transaction": {
-                "type": "0x02",
-                "from": "0x90f79bf6eb2c4f870365e785982e1f101e93b906",
-                "gas": "0x8f864",
-                "value": "0x0",
-                "data": "0x6080604",
-                "nonce": "0x0",
-                "accessList": []
-            },
-            "additionalContracts": []
-        }"#
-        .to_string();
-        let tx2 = r#"{
-            "hash": "0x5370406a7d060079764126708230356640e3494965321ab622842123ebb71052",
-            "transactionType": "CREATE",
-            "contractName": "PrivacyFactory",
-            "contractAddress": "0x261D8c5e9742e6f7f1076Fa1F560894524e19cad",
-            "function": null,
-            "arguments": null,
-            "transaction": {
-                "type": "0x02",
-                "from": "0x90f79bf6eb2c4f870365e785982e1f101e93b906",
-                "gas": "0x936a5",
-                "value": "0x0",
-                "data": "0x608060405",
-                "nonce": "0x1",
-                "accessList": []
-            },
-            "additionalContracts": []
-        }"#
-        .to_string();
-        let transactions_to_deserialize = TransactionsList {
-            transactions: vec![tx1, tx2],
-        };
-
-        let deserialized_tx1 = Transaction {
-            transaction_type: "CREATE".to_string(),
-            contract_address: "0x057ef64E23666F000b34aE31332854aCBd1c8544".to_string(),
-            transaction: TransactionDetails {
-                from: "0x90f79bf6eb2c4f870365e785982e1f101e93b906".to_string(),
-                to: None,
-                value: "0x0".to_string(),
-                data: "0x6080604".to_string(),
-            },
-        };
-        let deserialized_tx2 = Transaction {
-            transaction_type: "CREATE".to_string(),
-            contract_address: "0x261D8c5e9742e6f7f1076Fa1F560894524e19cad".to_string(),
-            transaction: TransactionDetails {
-                from: "0x90f79bf6eb2c4f870365e785982e1f101e93b906".to_string(),
-                to: None,
-                value: "0x0".to_string(),
-                data: "0x608060405".to_string(),
-            },
-        };
-
-        let expected_result = vec![deserialized_tx1, deserialized_tx2];
-
-        let deserialization_result =
-            deserialize_multiple_transactions(transactions_to_deserialize).unwrap();
-
-        assert_eq!(expected_result, deserialization_result);
-    }
-
-    #[test]
-    fn it_should_deserialize_a_series_of_receipts() {
-        let receipt1 = r#"{
-            "transactionHash": "0xd532ff21e93eac89c2bbd5f4813ac0d9274e479b6eb09b2b2f45b82489faba1b",
-            "transactionIndex": "0x0",
-            "blockHash": "0xec94f9df892826b801574831de293f983ed8f3f81036a99faa616a8da694b2a9",
-            "blockNumber": "0x1",
-            "from": "0x90F79bf6EB2c4f870365E785982E1f101E93b906",
-            "to": null,
-            "cumulativeGasUsed": "0x6e675",
-            "gasUsed": "0x6e675",
-            "contractAddress": "0x057ef64E23666F000b34aE31332854aCBd1c8544",
-            "logs": [
-                {
-                    "address": "0x057ef64E23666F000b34aE31332854aCBd1c8544",
-                    "topics": [
-                        "0x8be0079c531659141344cd1fd0a4f28419497f9722a3daafe3b4186f6b6457e0",
-                        "0x0000000000000000000000000000000000000000000000000000000000000000",
-                        "0x00000000000000000000000090f79bf6eb2c4f870365e785982e1f101e93b906"
-                    ],
-                    "data": "0x",
-                    "blockHash": "0xec94f9df892826b801574831de293f983ed8f3f81036a99faa616a8da694b2a9",
-                    "blockNumber": "0x1",
-                    "transactionHash": "0xd532ff21e93eac89c2bbd5f4813ac0d9274e479b6eb09b2b2f45b82489faba1b",
-                    "transactionIndex": "0x0",
-                    "logIndex": "0x0",
-                    "transactionLogIndex": "0x0",
-                    "removed": false
-                }
-            ],
-            "status": "0x1",
-            "logsBloom": "0x000000",
-            "effectiveGasPrice": "0xe0fed783"
-        }"#.to_string();
-        let receipt2 = r#"{
-            "transactionHash": "0x5370406a7d060079764126708230356640e3494965321ab622842123ebb71052",
-            "transactionIndex": "0x0",
-            "blockHash": "0x89faf9173c057b0db7693d1fbe9e06618ec69ad634ac5e0e42022f07c1ffc492",
-            "blockNumber": "0x2",
-            "from": "0x90F79bf6EB2c4f870365E785982E1f101E93b906",
-            "to": null,
-            "cumulativeGasUsed": "0x71658",
-            "gasUsed": "0x71658",
-            "contractAddress": "0x261D8c5e9742e6f7f1076Fa1F560894524e19cad",
-            "logs": [
-                {
-                    "address": "0x261D8c5e9742e6f7f1076Fa1F560894524e19cad",
-                    "topics": [
-                        "0x8be0079c531659141344cd1fd0a4f28419497f9722a3daafe3b4186f6b6457e0",
-                        "0x0000000000000000000000000000000000000000000000000000000000000000",
-                        "0x00000000000000000000000090f79bf6eb2c4f870365e785982e1f101e93b906"
-                    ],
-                    "data": "0x",
-                    "blockHash": "0x89faf9173c057b0db7693d1fbe9e06618ec69ad634ac5e0e42022f07c1ffc492",
-                    "blockNumber": "0x2",
-                    "transactionHash": "0x5370406a7d060079764126708230356640e3494965321ab622842123ebb71052",
-                    "transactionIndex": "0x0",
-                    "logIndex": "0x0",
-                    "transactionLogIndex": "0x0",
-                    "removed": false
-                }
-            ],
-            "status": "0x1",
-            "logsBloom": "0x0000",
-            "effectiveGasPrice": "0xe0fed783"
-        }"#.to_string();
-
-        let receipts_to_deserialize = ReceiptsList {
-            receipts: vec![receipt1, receipt2],
-        };
-
-        let deserialized_receipt1 = Receipt {
-            gas_used: "0x6e675".to_string(),
-            effective_gas_price: "0xe0fed783".to_string(),
-        };
-        let deserialized_receipt2 = Receipt {
-            gas_used: "0x71658".to_string(),
-            effective_gas_price: "0xe0fed783".to_string(),
-        };
-
-        let expected_result = vec![deserialized_receipt1, deserialized_receipt2];
-        let deserialization_result =
-            deserialize_multiple_receipts(receipts_to_deserialize).unwrap();
-
-        assert_eq!(expected_result, deserialization_result);
-    }
+    // #[test]
+    // fn it_should_deserialize_a_series_of_transactions() {
+    //     let tx1 = r#"{
+    //         "hash": "0xd532ff21e93eac89c2bbd5f4813ac0d9274e479b6eb09b2b2f45b82489faba1b",
+    //         "transactionType": "CREATE",
+    //         "contractName": "Ethernaut",
+    //         "contractAddress": "0x057ef64E23666F000b34aE31332854aCBd1c8544",
+    //         "function": null,
+    //         "arguments": null,
+    //         "transaction": {
+    //             "type": "0x02",
+    //             "from": "0x90f79bf6eb2c4f870365e785982e1f101e93b906",
+    //             "gas": "0x8f864",
+    //             "value": "0x0",
+    //             "data": "0x6080604",
+    //             "nonce": "0x0",
+    //             "accessList": []
+    //         },
+    //         "additionalContracts": []
+    //     }"#
+    //     .to_string();
+    //     let tx2 = r#"{
+    //         "hash": "0x5370406a7d060079764126708230356640e3494965321ab622842123ebb71052",
+    //         "transactionType": "CREATE",
+    //         "contractName": "PrivacyFactory",
+    //         "contractAddress": "0x261D8c5e9742e6f7f1076Fa1F560894524e19cad",
+    //         "function": null,
+    //         "arguments": null,
+    //         "transaction": {
+    //             "type": "0x02",
+    //             "from": "0x90f79bf6eb2c4f870365e785982e1f101e93b906",
+    //             "gas": "0x936a5",
+    //             "value": "0x0",
+    //             "data": "0x608060405",
+    //             "nonce": "0x1",
+    //             "accessList": []
+    //         },
+    //         "additionalContracts": []
+    //     }"#
+    //     .to_string();
+    //     let transactions_to_deserialize = TransactionsList {
+    //         transactions: vec![tx1, tx2],
+    //     };
+    //
+    //     let deserialized_tx1 = Transaction {
+    //         transaction_type: "CREATE".to_string(),
+    //         contract_address: "0x057ef64E23666F000b34aE31332854aCBd1c8544".to_string(),
+    //         transaction: TransactionDetails {
+    //             from: "0x90f79bf6eb2c4f870365e785982e1f101e93b906".to_string(),
+    //             to: None,
+    //             value: "0x0".to_string(),
+    //             data: "0x6080604".to_string(),
+    //         },
+    //     };
+    //     let deserialized_tx2 = Transaction {
+    //         transaction_type: "CREATE".to_string(),
+    //         contract_address: "0x261D8c5e9742e6f7f1076Fa1F560894524e19cad".to_string(),
+    //         transaction: TransactionDetails {
+    //             from: "0x90f79bf6eb2c4f870365e785982e1f101e93b906".to_string(),
+    //             to: None,
+    //             value: "0x0".to_string(),
+    //             data: "0x608060405".to_string(),
+    //         },
+    //     };
+    //
+    //     let expected_result = vec![deserialized_tx1, deserialized_tx2];
+    //
+    //     let deserialization_result =
+    //         deserialize_multiple_transactions(transactions_to_deserialize).unwrap();
+    //
+    //     assert_eq!(expected_result, deserialization_result);
+    // }
+    //
+    // #[test]
+    // fn it_should_deserialize_a_series_of_receipts() {
+    //     let receipt1 = r#"{
+    //         "transactionHash": "0xd532ff21e93eac89c2bbd5f4813ac0d9274e479b6eb09b2b2f45b82489faba1b",
+    //         "transactionIndex": "0x0",
+    //         "blockHash": "0xec94f9df892826b801574831de293f983ed8f3f81036a99faa616a8da694b2a9",
+    //         "blockNumber": "0x1",
+    //         "from": "0x90F79bf6EB2c4f870365E785982E1f101E93b906",
+    //         "to": null,
+    //         "cumulativeGasUsed": "0x6e675",
+    //         "gasUsed": "0x6e675",
+    //         "contractAddress": "0x057ef64E23666F000b34aE31332854aCBd1c8544",
+    //         "logs": [
+    //             {
+    //                 "address": "0x057ef64E23666F000b34aE31332854aCBd1c8544",
+    //                 "topics": [
+    //                     "0x8be0079c531659141344cd1fd0a4f28419497f9722a3daafe3b4186f6b6457e0",
+    //                     "0x0000000000000000000000000000000000000000000000000000000000000000",
+    //                     "0x00000000000000000000000090f79bf6eb2c4f870365e785982e1f101e93b906"
+    //                 ],
+    //                 "data": "0x",
+    //                 "blockHash": "0xec94f9df892826b801574831de293f983ed8f3f81036a99faa616a8da694b2a9",
+    //                 "blockNumber": "0x1",
+    //                 "transactionHash": "0xd532ff21e93eac89c2bbd5f4813ac0d9274e479b6eb09b2b2f45b82489faba1b",
+    //                 "transactionIndex": "0x0",
+    //                 "logIndex": "0x0",
+    //                 "transactionLogIndex": "0x0",
+    //                 "removed": false
+    //             }
+    //         ],
+    //         "status": "0x1",
+    //         "logsBloom": "0x000000",
+    //         "effectiveGasPrice": "0xe0fed783"
+    //     }"#.to_string();
+    //     let receipt2 = r#"{
+    //         "transactionHash": "0x5370406a7d060079764126708230356640e3494965321ab622842123ebb71052",
+    //         "transactionIndex": "0x0",
+    //         "blockHash": "0x89faf9173c057b0db7693d1fbe9e06618ec69ad634ac5e0e42022f07c1ffc492",
+    //         "blockNumber": "0x2",
+    //         "from": "0x90F79bf6EB2c4f870365E785982E1f101E93b906",
+    //         "to": null,
+    //         "cumulativeGasUsed": "0x71658",
+    //         "gasUsed": "0x71658",
+    //         "contractAddress": "0x261D8c5e9742e6f7f1076Fa1F560894524e19cad",
+    //         "logs": [
+    //             {
+    //                 "address": "0x261D8c5e9742e6f7f1076Fa1F560894524e19cad",
+    //                 "topics": [
+    //                     "0x8be0079c531659141344cd1fd0a4f28419497f9722a3daafe3b4186f6b6457e0",
+    //                     "0x0000000000000000000000000000000000000000000000000000000000000000",
+    //                     "0x00000000000000000000000090f79bf6eb2c4f870365e785982e1f101e93b906"
+    //                 ],
+    //                 "data": "0x",
+    //                 "blockHash": "0x89faf9173c057b0db7693d1fbe9e06618ec69ad634ac5e0e42022f07c1ffc492",
+    //                 "blockNumber": "0x2",
+    //                 "transactionHash": "0x5370406a7d060079764126708230356640e3494965321ab622842123ebb71052",
+    //                 "transactionIndex": "0x0",
+    //                 "logIndex": "0x0",
+    //                 "transactionLogIndex": "0x0",
+    //                 "removed": false
+    //             }
+    //         ],
+    //         "status": "0x1",
+    //         "logsBloom": "0x0000",
+    //         "effectiveGasPrice": "0xe0fed783"
+    //     }"#.to_string();
+    //
+    //     let receipts_to_deserialize = ReceiptsList {
+    //         receipts: vec![receipt1, receipt2],
+    //     };
+    //
+    //     let deserialized_receipt1 = Receipt {
+    //         gas_used: "0x6e675".to_string(),
+    //         effective_gas_price: "0xe0fed783".to_string(),
+    //     };
+    //     let deserialized_receipt2 = Receipt {
+    //         gas_used: "0x71658".to_string(),
+    //         effective_gas_price: "0xe0fed783".to_string(),
+    //     };
+    //
+    //     let expected_result = vec![deserialized_receipt1, deserialized_receipt2];
+    //     let deserialization_result =
+    //         deserialize_multiple_receipts(receipts_to_deserialize).unwrap();
+    //
+    //     assert_eq!(expected_result, deserialization_result);
+    // }
     #[test]
     fn it_should_deserialize_broadcast() {
         let broadcast_to_deserialize = r#"{
@@ -553,12 +545,12 @@ mod parser_tests {
 }"#;
         let expected_tx1 = Transaction {
             transaction_type: "CREATE".to_string(),
-            contract_address: "0x057ef64E23666F000b34aE31332854aCBd1c8544".to_string(),
+            contract_address: "0x261D8c5e9742e6f7f1076Fa1F560894524e19cad".to_string(),
             transaction: TransactionDetails {
                 from: "0x90f79bf6eb2c4f870365e785982e1f101e93b906".to_string(),
                 to: None,
                 value: "0x0".to_string(),
-                data: "0x6080604".to_string(),
+                data: "0x608060405".to_string(),
             },
         };
 
@@ -577,17 +569,17 @@ mod parser_tests {
             effective_gas_price: "0xe0fed783".to_string(),
         };
         let expected_receipt2 = Receipt {
-            gas_used: "0x71658".to_string(),
-            effective_gas_price: "0xb3bd".to_string(),
+            gas_used: "0xb3bd".to_string(),
+            effective_gas_price: "0xe0fed783".to_string(),
         };
-        let transactions = TransactionsList {
-            transactions: vec![expected_tx1, expected_tx2],
-        };
-        let receipts = ReceiptsList {
-            receipts: vec![expected_receipt1, expected_receipt2],
+        let transactions = vec![expected_tx1, expected_tx2];
+        let receipts = vec![expected_receipt1, expected_receipt2];
+        let expected_broadcast = Broadcast {
+            transactions,
+            receipts,
         };
         let deserialization_result = deserialize_broadcast(broadcast_to_deserialize).unwrap();
-        assert_eq!((transactions, receipts), deserialization_result);
+        assert_eq!(expected_broadcast, deserialization_result);
     }
     /*//////////////////////////////////////////////////////////////
                             SERIALIZATION TESTS
