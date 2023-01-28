9fd956be066632155f043606a99dfb7253672171
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-01-28 21:17:01 +0100

    chore: add error handling to main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use foundry2echidna::run;
+use std::process;
 
 fn main() {
     run(
         "test_json_files/test_broadcast.json",
         "test_json_files/test_broadcast_transformed.json",
-    );
+    )
+    .unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 }
