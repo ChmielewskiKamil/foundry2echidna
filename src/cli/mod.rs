1c6f9ff492ca7c2a95b90220a175c90de809ba4e
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-01-29 00:23:12 +0100

    chore: add clap for CLI
    chore: add derive feature for clap
    
    chore: WIP CLI

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -2,6 +2,8 @@ use crate::deserialization::deserialize_broadcast;
 use crate::file_handling::{read_broadcast_file, write_transformed_broadcast_to_file};
 use crate::serialization::serialize_broadcast;
 
+use clap::Parser;
+
 pub fn run(input_path: &str, output_path: &str) -> Result<(), String> {
     let broadcast_to_deserialize = read_broadcast_file(input_path)?;
     let broadcast = deserialize_broadcast(&broadcast_to_deserialize)?;
@@ -9,3 +11,11 @@ pub fn run(input_path: &str, output_path: &str) -> Result<(), String> {
     write_transformed_broadcast_to_file(broadcast, output_path)?;
     Ok(())
 }
+
+#[derive(Parser, Debug)]
+#[clap(
+    name = "Foundry to Echidna",
+    about = "Easily seed Echidna with Foundry broadcast files."
+)]
+
+pub struct Args {}
