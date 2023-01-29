64edb7474b01e1fc82d1ac9e0687540103b62f24
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-01-29 14:51:37 +0100

    refactor: handle errors in idiomatic way

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ use foundry2echidna::cli::{run, Args};
 fn main() {
     let args = Args::new();
 
-    match run(
+    if let Err(e) = run(
         args.input_path.as_ref().unwrap(),
         args.output_path.as_ref().unwrap(),
     ) {
-        Ok(_) => println!("Success"),
-        Err(e) => eprintln!("Error: {}", e),
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
     }
+
+    println!("Success");
 }
