f721b46e8cf790e3d2512f7bc2de354aa3b5d6a5
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-01-30 16:10:51 +0100

    refactor: rename public API function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use foundry2echidna::cli::{run, Args};
+use foundry2echidna::cli::{transform_broadcast, Args};
 
 fn main() {
     Args::new()
         .map(|args| (args.input_path.unwrap(), args.output_path.unwrap()))
-        .and_then(|(input_path, output_path)| run(&input_path, &output_path))
+        .and_then(|(input_path, output_path)| transform_broadcast(&input_path, &output_path))
         .map(|_| println!("Transformed broadcast successfully!"))
         .unwrap_or_else(|e| {
             eprintln!("Error: {e}");
